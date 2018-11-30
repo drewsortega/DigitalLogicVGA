b5cac660fab9bd9497b195997fae074cce9645dd
Orvyx <DrewSOrtega@gmail.com>
2018-11-30 01:22:34 -0800

    finish Top_Level.sv hookups

--- Top_Level.sv
+++ Top_Level.sv
@@ -1,18 +1,71 @@
 module Top_Level (
-    input logic CLK,
-    input logic[1:0] Choice_Player1,
-    input logic[1:0] Choice_Player2,
-);
-logic X1, Y1, X2, Y2;
+    input logic Clock,
 
-//TODO: all of this 
-Player player1(.CLK(CLK), .X(X1), .Y(Y1));
-Player player2(.CLK(CLK), .X(X2), .Y(Y2));
+    //player 1 inputs
+    input logic[1:0] Choice_Player1,
+    input logic IRData1,
+    input logic PS2Data1,
+    input logic SNESData1,
+
+    //player 2 inputs
+    input logic[1:0] Choice_Player2,
+    input logic IRData2,
+    input logic PS2Data2,
+    input logic SNESData2,
+
+    //player 1 outputs to SNES/NES controller
+    output logic NStrobe_Latch1,
+    output logic NShift_Clock1,
+
+    //player 2 outputs to SNES/NES controller
+    output logic NStrobe_Latch2,
+    output logic NShift_Clock2,
+
+    //video output
+    output logic H_Sync,
+    output logic V_Sync,
+    output logic Red,
+    output logic Green,
+    output logic Blue
+);
+logic X1, Y1, X2, Y2, XDraw, YDraw, No_Color;
+
+Player player1(.Clock(Clock), 
+    .IRData(IRData1), 
+    .PS2Data(PS2Data1), 
+    .SNESData(SNESData1),
+    .X(X1), 
+    .Y(Y1)
+);
+
+Player player2(.Clock(Clock), 
+    .IRData(IRData2), 
+    .PS2Data(PS2Data2), 
+    .SNESData(SNESData2),
+    .X(X1), 
+    .Y(Y1)
+);
+
+XYCounter counter(
+    .clock(Clock),
+    .xcoord(XDraw),
+    .ycoord(YDraw),
+    .hsync(H_Sync),
+    .vsync(V_Sync),
+    .nocolor(No_Color)
+);
 
 Renderer renderer(
-    .X1(X1),
-    .Y1(Y1),
-    .X2(X2),
-    .Y2(Y2)
+    .xcoord(XDraw),
+    .ycoord(YDraw),
+    .nocolor(No_Color),
+    .p1x(X1),
+    .p1y(Y1),
+    .p2x(X2),
+    .p2y(Y2),
+    .red(Red),
+    .blue(Blue),
+    .green(Green)
 );
+
 endmodule
